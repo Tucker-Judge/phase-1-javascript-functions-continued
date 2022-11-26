9409516fe78133eb95559f0d7b7a4c110e1bdd3e
Tucker <tudge669@gmail.com>
2022-11-25 19:36:54 -0500

    commit tree 00d6c294ef92cd10d35fc35dddccf6a4b0b77132

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+// **LAB**: Implement a function called `saturdayFun`. It should return a `String`
+// like `"This Saturday, I want to ....!"` Fill in the `...` with the activity
+// that's passed in as the first parameter. If nothing is passed in, default to
+// `"roller-skate"`. Run `learn test` to verify you've gotten the first set of
+// tests passing before continuing with the lesson.
+
+function saturdayFun(activity='roller-skate') {
+    return "This Saturday, I want to " + activity+ "!"
+}
+function mondayWork(activity='go to the office') {
+    return "This Monday, I will " + activity+ "."
+}
+// function wrapAdjective(highlight) {
+//         // return function(adjective) {
+//         //     "You are "+ highlight + adjective + highlight + "!"
+//         // }(
+       
+        
+//         return function (adjective) {
+//                 "You are "+ highlight + adjective + highlight + "!"
+//         }
+//         }
+
+// function wrapAdjective(highlight) {
+//     return function (adjective) {
+//     return function () {
+//     'You are ' + highlight+adjective+highlight + '!'
+//               };
+//             };
+//           }
+function wrapAdjective(x) {
+    function inside(y) {
+        return `You are ${x}${y}${x}!`;
+    }
+    return inside;
+}
+
+    
+
